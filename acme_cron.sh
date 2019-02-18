6e804f64528ef73e0ce6078131c6db2e86922de3
David Robles <master-nevi@users.noreply.github.com>
2019-02-17 16:27:50 -0800

    Update variable name.

--- acme_cron.sh
+++ acme_cron.sh
@@ -14,19 +14,19 @@ check_var CONFIG_FILE
 # Check vars from sourced config file.
 check_var ACME_DOMAIN
 check_var ACME_DNSAPI
-check_var ACME_WORKINGDIR
+check_var ACME_WORKING_DIR
 
-cd $ACME_WORKINGDIR
+cd $ACME_WORKING_DIR
 rm -rf master.tar.gz 2> /dev/null
 rm -rf acme.sh-master 2> /dev/null
 rm -rf acme.sh 2> /dev/null
 wget https://github.com/Neilpang/acme.sh/archive/master.tar.gz
 tar xvf master.tar.gz
 cd acme.sh-master
-./acme.sh --install --nocron --home "$ACME_WORKINGDIR/acme.sh"
+./acme.sh --install --nocron --home "$ACME_WORKING_DIR/acme.sh"
 cd ..
 cd acme.sh
-./acme.sh --issue --post-hook "/usr/syno/sbin/synoservicecfg --restart httpd-sys" --dns $ACME_DNSAPI --certpath /usr/syno/etc/ssl/ssl.crt/server.crt --keypath /usr/syno/etc/ssl/ssl.key/server.key --ca-file /usr/syno/etc/ssl/ssl.intercrt/server-ca.crt --config-home "$ACME_WORKINGDIR/acme.sh/" --dnssleep 300 -d $ACME_DOMAIN --debug
+./acme.sh --issue --post-hook "/usr/syno/sbin/synoservicecfg --restart httpd-sys" --dns $ACME_DNSAPI --certpath /usr/syno/etc/ssl/ssl.crt/server.crt --keypath /usr/syno/etc/ssl/ssl.key/server.key --ca-file /usr/syno/etc/ssl/ssl.intercrt/server-ca.crt --config-home "$ACME_WORKING_DIR/acme.sh/" --dnssleep 300 -d $ACME_DOMAIN --debug
 cd ..
 rm -rf master.tar.gz 2> /dev/null
 rm -rf acme.sh-master 2> /dev/null
